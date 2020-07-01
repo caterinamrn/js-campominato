c729ffdc063afdf0e2e553ccf0cf4dee0fb7ad34
caterina <catemarian1806@gmail.com>
2020-07-01 18:02:29 +0200

    aggiungo condizioni

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,7 +1,3 @@
-// mi creo variabili per inserire il numero massimo di numeri richiesti così che se devo cambiarli posso cambiarli velocemente
-// utilizzo cicli while visto che non so quante ripetizioni sono necessarie anche se conosco il numero finale a cui devo arrivare
-// utilizzo di functions mi semplifica il codice, posso utilizzarle più volte e semplificano il codice, soprattuto gli if -in questo caso- avendo a disposizioni delle condizioni create dalle functions
-
 
   var numComputer = [];
   var maxNumComputer = 16;
@@ -19,9 +15,9 @@
   console.log(numComputer);
 
 // chiedo all'utente un numero da 1 a 100 fino a quando non ne scrive uno uguale a quelli generati in numComputer
- while (numUtente.length < maxNumUtente && !(inclusoInArray(numComputer,num))  ) {
+ while (numUtente.length < maxNumUtente && !(inclusoInArray(numComputer,num))) {
     num = parseInt(prompt("Inserire un numero da 1 a 100"));
-    if (!(inclusoInArray(numUtente,num)) && !(inclusoInArray(numComputer,num)) && condizioni(100,1,num)== true ) {
+    if (!(inclusoInArray(numUtente,num)) && !(inclusoInArray(numComputer,num))) {
       numUtente.push(num);
     }
 }
@@ -34,30 +30,9 @@ else {
   console.log("Hai perso!");
 }
 console.log("il tuo punteggio è di", numUtente.length);
-// for (var i = 0; i < 16; i++) {
-//   numComputer.push(getRandomIntInclusive(1,100));
-// }
-// console.log(numComputer);
-// var i = 0;
-// var trovato;
-// do {
-//   var num = parseInt(prompt("Inserire un numero da 1 a 100"));
-//   trovato = numComputer.includes(num);
-//   i++;
-//   console.log(trovato);
-// } while (trovato == false && i < 84 );
-// if (trovato== false) {
-//   console.log("Hai vinto");
-// } else {
-//   console.log("Hai perso", i);
-// }
 
-// while (arraynum.length < numComputerMax) {
-//   numRandom = getRandomIntInclusive(1,10);
-//   if (inclusoInArray(numComputer,numRandom) == false) {
-//     numRandom.push(numComputer);
-//   }
-// }
+
+
 
 // functions
 // generatore numeri randomici
@@ -69,12 +44,6 @@ function getRandomIntInclusive(min,max) {
 }
 // devo creare una function per vedere se c'è una ripetizione(sia per i numeri random che per i numeri inseriti dall'utente)che sostituisce includes
 function inclusoInArray (array,num){
-  // for (var i = 0; i < array.length; i++) {
-  //   if (num == array[i]) {
-  //     return false;
-  //   }
-  //   return true;
-  // }
   var i = 0;
   var trovato = false;
   while (i < array.length && trovato == false) {
@@ -87,7 +56,6 @@ function inclusoInArray (array,num){
 }
 // devo creare una function per vedere se il numero inserito è nel range e se è un numero
 function condizioni(max,min,num){
-  // console.log(num < min,num > max,isNaN(num),num < min || num > max || !isNaN(num));
   var incluso = false;
   if (num < min || num > max || isNaN(num)) {
     return incluso = false;
